7b57ab73ac1a03d7afeb74c2811f46578041a486
Hong Yehee <ghddpgml@gmail.com>
2025-05-31 23:52:52 +0900

    9강: 주사위 앱

---
+++ 02-DiceGame/DiceGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceGame
+//
+//  Created by 홍예희 on 5/31/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
