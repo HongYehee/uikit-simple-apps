9890f2b05cf683b1a5c4e85caef46018304f6dfb
Hong Yehee <ghddpgml@gmail.com>
2025-06-04 16:53:11 +0900

    11강: 가위바위보 앱

--- 02-DiceGame/DiceGame/ViewController.swift
+++ 02-DiceGame/DiceGame/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var firstImageView: UIImageView!
+    @IBOutlet weak var secondImageView: UIImageView!
+    // 6가지 주사위 이미지를 담기 위한 array
+    var diceArray: [UIImage] = [#imageLiteral(resourceName: "black1"), #imageLiteral(resourceName: "black2"), #imageLiteral(resourceName: "black3"), #imageLiteral(resourceName: "black4"), #imageLiteral(resourceName: "black5"), #imageLiteral(resourceName: "black6")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        firstImageView.image = diceArray[0]
+        secondImageView.image = diceArray[0]
     }
 
+    @IBAction func rollButtonTapped(_ sender: UIButton) {
+        // 첫 번째 이미지뷰의 이미지를 랜덤으로 변경
+        firstImageView.image = diceArray.randomElement()
+        // 두 번째 이미지뷰의 이미지를 랜덤으로 변경
+        secondImageView.image = diceArray.randomElement()
+    }
 
 }
 
